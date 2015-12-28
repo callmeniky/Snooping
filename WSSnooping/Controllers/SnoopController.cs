523d50139057824acd68ab3656f455f9c4588871
nemz27 <niskeydimichel@gmail.com>
2015-12-28 01:44:04 -0400

    Add: Agregando métodos al controller de WebApi

--- WSSnooping/Controllers/SnoopController.cs
+++ WSSnooping/Controllers/SnoopController.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WSSnooping.Infraestructure.ViewModels;
+using WSSnooping.Infraestructure.InputModels;
+using WSSnooping.Models.Repositorios;
+using WSSnooping.Models.Repositorios.Servicios;
 
 namespace WSSnooping.Controllers
 {
-    public class SnoopController : Controller
+    public class SnoopController : ApiController
     {
-        //
-        // GET: /Snoop/
+        private Snoop servSnoop;
 
-        public ActionResult Index()
+        public SnoopController()
         {
-            return View();
+            this.servSnoop = new Snoop();
         }
 
+        public List<SnoopViewModel> GetEntradasUsuariosSeguidos(int usuarioId)
+        {
+            List<SnoopViewModel> lstSnoopVw = new List<SnoopViewModel>();
+
+            lstSnoopVw = servSnoop.ListarSnoops(usuarioId);
+
+            return lstSnoopVw;
+        }
+
+        public bool PutSnoop(SnoopInputModel snoopInput)
+        {
+            bool result = false;
+
+            return result;
+        }
     }
 }
