4d09b4a2dfea23326f1403d82f0ea3927a25832a
nemz27 <niskeydimichel@gmail.com>
2015-12-28 01:12:55 -0400

    Add: +Agregando controlador "Snoop" en WS

---
+++ WSSnooping/Controllers/SnoopController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WSSnooping.Controllers
+{
+    public class SnoopController : Controller
+    {
+        //
+        // GET: /Snoop/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
